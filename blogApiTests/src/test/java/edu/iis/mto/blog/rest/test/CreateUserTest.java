f6076e63176167a3af4fd4dda9112e4eb107eaf8
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-05-30 19:50:50 +0200

    3.4 first test

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -11,7 +11,20 @@ import io.restassured.http.ContentType;
 class CreateUserTest extends FunctionalTests {
 
     private static final String USER_API = "/blog/user";
+    @Test
 
+    void createUserWithEmailThatIsInDataBaseShouldReturn() {
+        JSONObject jsonObj = new JSONObject().put("email", "t12@domain.com");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect()
+                .log().all().statusCode(HttpStatus.SC_CREATED).when().post(USER_API);
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect().log().all()
+                .statusCode(HttpStatus.SC_CONFLICT).when().post(USER_API);
+    }
     @Test
     void createUserWithProperDataReturnsCreatedStatus() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
